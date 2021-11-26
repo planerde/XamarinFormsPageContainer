53b35c9e96ec96ec884e0174c1662169b910ca1d
Tim Preuß <dev@timpreuss.de>
2021-11-26 16:34:07 +0100

    adds AdaptLayout method to be able to try out different screen size changes (tablet mode?)

--- ShellExample/Views/AboutPage.xaml.cs
+++ ShellExample/Views/AboutPage.xaml.cs
@@ -11,5 +11,35 @@ namespace ShellExample.Views
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            this.SizeChanged += Page_SizeChanged;
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            this.SizeChanged -= Page_SizeChanged;
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            AdaptLayout();
+        }
+
+        private void Page_SizeChanged(object sender, EventArgs e)
+        {
+            AdaptLayout();
+        }
+
+        private void AdaptLayout()
+        {
+            string visualState = (Device.Idiom == TargetIdiom.Tablet && Width > Height) ? "Landscape" : "Portrait";
+
+            VisualStateManager.GoToState(MainContent, visualState);
+        }
     }
 }
