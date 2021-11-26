aaf726b80ff0cf5835a7fec2b79a6e0876fbf467
Tim Preuß <dev@timpreuss.de>
2021-11-26 16:09:44 +0100

    initial import

---
+++ ShellExample/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using ShellExample.ViewModels;
+
+namespace ShellExample.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
